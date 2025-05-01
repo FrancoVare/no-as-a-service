8d86f41bc06bea3ef3312d07ca7293435834de0b
FrancoVare <vare.franco@gmail.com>
2025-05-01 13:28:45 -0300

    fix fetch data before listen

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const RATE_LIMIT_MAX = Number(process.env.RATE_LIMIT_MAX) || 10;
 const RATE_LIMIT_WINDOW_MINUTES = Number(process.env.RATE_LIMIT_WINDOW_MS) || 1;
 const REASONS_URL = process.env.REASONS_URL || 'https://raw.githubusercontent.com/hotheadhacker/no-as-a-service/refs/heads/main/reasons.json';
 
+var reasons = [];
+
 // Trust proxies for rate limiting
 app.set('trust proxy', TRUST_PROXIES);
 
@@ -43,21 +45,29 @@ app.get('/', (req, res) => {
 app.use('/frontend', express.static('frontend/public'));
 
 // Load reasons from JSON
-const response = await fetch(REASONS_URL);
-const reasons = [];
+async function fetchData() {
+  const response = await fetch(REASONS_URL);
 
-if (!response.ok) {
-  console.error(`Failed to fetch reasons: ${response.statusText}, using local file as fallback`);
-  reasons = JSON.parse(fs.readFileSync('./reasons.json', 'utf-8'));
-} else {
-  console.log(`Fetched reasons from ${REASONS_URL}`);
-  const reasonsJson = await response.json();
-  reasons = reasonsJson.reasons;
+  if (!response.ok) {
+    console.error(`Failed to fetch reasons: ${response.statusText}, using local file as fallback`);
+    reasons = JSON.parse(fs.readFileSync('./reasons.json', 'utf-8'));
+  } else {
+    console.log(`Fetched reasons from ${REASONS_URL}`);
+    const reasonsJson = await response.text();
+    reasons = JSON.parse(reasonsJson);
+  }
 }
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`No-as-a-Service is running on port ${PORT}`);
-  console.log('Log format:');
-  console.log(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"');
-});
+function startServer() {
+  fetchData()
+    .then(() => {
+      app.listen(PORT, () => {
+        console.log(`No-as-a-Service is running on port ${PORT}`);
+        console.log('Log format:');
+        console.log(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"');
+      });
+    });
+}
+
+// Start the server
+startServer();
